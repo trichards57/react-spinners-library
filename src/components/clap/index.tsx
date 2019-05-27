bf216aaeb92f64c947600daf07a455b07c9ac517
Tony Richards <trichards57@gmail.com>
2019-05-27 15:09:57 +0100

    Further slimming of the clap spinner component.

--- src/components/clap/index.tsx
+++ src/components/clap/index.tsx
@@ -8,19 +8,21 @@ export const ClapSpinner = ({
   color = '#00ff89',
   loading = true
 }: IComponentProps) => {
+  const items = [...Array(7).keys()].map(i => (
+    <div
+      className={classNames(styles[`ball-${i}`], styles.ball)}
+      style={{ backgroundColor: color }}
+      key={i}
+    />
+  ));
+
   return (
     loading && (
       <div
         className={styles.wrapper}
         style={{ transform: `scale(${size / 40})` }}
       >
-        <div className={classNames(styles["ball-0"], styles.ball)} style={{ backgroundColor: color }} />
-        <div className={classNames(styles["ball-1"], styles.ball)} style={{ backgroundColor: color }} />
-        <div className={classNames(styles["ball-2"], styles.ball)} style={{ backgroundColor: color }} />
-        <div className={classNames(styles["ball-3"], styles.ball)} style={{ backgroundColor: color }} />
-        <div className={classNames(styles["ball-4"], styles.ball)} style={{ backgroundColor: color }} />
-        <div className={classNames(styles["ball-5"], styles.ball)} style={{ backgroundColor: color }} />
-        <div className={classNames(styles["ball-6"], styles.ball)} style={{ backgroundColor: color }} />
+        {items}
       </div>
     )
   );
