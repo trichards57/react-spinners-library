29175ea64c002e50162191ccad15cb6fc0e3c762
Tony Richards <trichards57@gmail.com>
2019-05-27 15:02:08 +0100

    Fixed and slimmed down bars component.

--- src/components/bars/index.tsx
+++ src/components/bars/index.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React from 'react';
 import { componentProps, IComponentProps } from '../component-props';
 import styles from './bars.module.scss';
@@ -7,17 +8,21 @@ export const BarsSpinner = ({
   color = '#00ff89',
   loading = true
 }: IComponentProps) => {
+  const items = [...Array(5).keys()].map(i => (
+    <div
+      className={classNames(styles[`bar-${i}`], styles.bar)}
+      style={{ backgroundColor: color }}
+      key={i}
+    />
+  ));
+
   return (
     loading && (
       <div
         className={styles.wrapper}
         style={{ transform: `scale(${size / 40})` }}
       >
-        <div className={styles.bar0} style={{ backgroundColor: color }} />
-        <div className={styles.bar1} style={{ backgroundColor: color }} />
-        <div className={styles.bar2} style={{ backgroundColor: color }} />
-        <div className={styles.bar3} style={{ backgroundColor: color }} />
-        <div className={styles.bar4} style={{ backgroundColor: color }} />
+        {items}
       </div>
     )
   );
