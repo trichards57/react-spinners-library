ef041bdfc495a41332ad87632e703a7b2850180e
Tony Richards <trichards57@gmail.com>
2019-05-27 15:49:50 +0100

    Updated packages.

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import resolve from 'rollup-plugin-node-resolve';
 import url from 'rollup-plugin-url';
-import svgr from '@svgr/rollup';
 import visualizer from 'rollup-plugin-visualizer';
 
 import pkg from './package.json';
@@ -31,7 +30,6 @@ export default {
       modules: true
     }),
     url(),
-    svgr(),
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
